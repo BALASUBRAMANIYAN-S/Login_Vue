24b473398e4586a44b697a6c10bb1f69cb66c642
BALASUBRAMANIYAN-S <balalatha660@gmail.com>
2026-02-10 09:57:06 +0530

    Create a Users Login & Register API

---
+++ backend/users/views.py
@@ -0,0 +1,30 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer
+
+# REGISTER (CREATE)
+class RegisterAPI(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User created"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=400)
+
+
+# LOGIN
+class LoginAPI(APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+
+        if user:
+            return Response({"message": "Login success"})
+        return Response({"error": "Invalid credentials"}, status=401)
